605c0c04beac9b864b6e6868afd9dfad88c939c8
Abderrahim T <46535711+leaderbear@users.noreply.github.com>
2021-07-05 21:30:11 +0000

    bot uses an api to answer with inspiration msg when user say "j'en ai marre" in server

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
 import discord
+import requests
+import json 
 
 client = discord.Client()
 
+# this function use an api to get random quote and convert it into json
+# not really useful for the bot, but can be used as an exemple for later
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+# confirms that the your bot logged in with success
 @client.event
 async def on_ready(): 
   print('We have logged in as {0.user}'.format(client)) 
 
+# liste to other users messages and answers in the same channel
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.content.startwith('$hello'):
-    await message.channel.send("hello toi-même")
+  if message.content.startswith('j\'en ai marre'):
+    quote = get_quote()
+    await message.channel.send(quote)
 
 
-# this line runs the bot  
+# this line runs the bot , replace by your own bot token
 client.run(os.environ['TOKEN'])
